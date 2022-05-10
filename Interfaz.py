4162035ba941b55653cbd841ffea8b2beb6914c7
Gar8Esteban <84606270+Gar8Esteban@users.noreply.github.com>
2022-05-09 20:44:18 -0500

    Interfaz con colores

--- Interfaz.py
+++ Interfaz.py
@@ -1,10 +1,14 @@
 import tkinter as tk
+from tkinter.constants import LEFT, BOTTOM, TOP
 
 
 class gui(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Algoritmo')
+        self.geometry('900x600')
+        mainPanel = tk.Frame(self, width=900, height=600, bg='lightblue')
+        mainPanel.pack(side=TOP)
 
 
 if __name__ == '__main__':
