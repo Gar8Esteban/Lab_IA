489504aabbed4c92c8815e4c6ed5a6023643fdc8
Gar8Esteban <84606270+Gar8Esteban@users.noreply.github.com>
2022-05-09 21:13:57 -0500

    Botnes de ingreso y borrar

--- Interfaz.py
+++ Interfaz.py
@@ -8,7 +8,31 @@ class gui(tk.Tk):
         self.title('Algoritmo')
         self.geometry('900x600')
         mainPanel = tk.Frame(self, width=900, height=600, bg='lightblue')
-        mainPanel.pack(side=TOP)
+        mainPanel.grid(row=0, column=0)
+
+        labelName1 = tk.Label(mainPanel, text='Esteban García Ochoa-1803291', bg='lightblue')
+        labelName1.grid(row=0, column=0)
+
+        labelName2 = tk.Label(mainPanel, text='Duvan José Botia-18032xx', bg='lightblue')
+        labelName2.grid(row=0, column=1)
+
+        labelXi = tk.Label(mainPanel, text='Xi', bg='lightblue')
+        labelXi.grid(row=1, column=0)
+
+        entryXi = tk.Entry(mainPanel)
+        entryXi.grid(row=1, column=1)
+
+        labelXf = tk.Label(mainPanel, text='Xf', bg='lightblue')
+        labelXf.grid(row=2, column=0)
+
+        entryXf = tk.Entry(mainPanel)
+        entryXf.grid(row=2, column=1)
+
+        buttonEntry = tk.Button(mainPanel, text='Ingresar', bg='green')
+        buttonEntry.grid(row=3, column=0)
+
+        buttonDelete = tk.Button(mainPanel, text='Borrar', bg='red')
+        buttonDelete.grid(row=3, column=1)
 
 
 if __name__ == '__main__':
